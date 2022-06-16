c94efd9ab47b5ec584c9f75ca6569f6134368744
katzavben <katzavben@gmail.com>
2022-06-16 03:51:07 +0300

    updated backend block

--- providers.tf
+++ providers.tf
@@ -1,16 +1,16 @@
 # Configure the Azure provider
 terraform {
   # The following backend block is used in order to store the Terraform state in Azure Blob Storage
-  # In case this is the first init please comment the backend block
-  # For the reason that the storage account has not been created yet
-
+  # Please uncomment the backend block in order to use it
+  # ONLY DO IT IF IT'S NOT THE FIRST INIT AND STORAGE ACCOUNT ALREADY CREATED
+  /*
   backend "azurerm" {
     resource_group_name  = "weight-tracker"
     storage_account_name = "benkatzavstoragetfstate"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
-
+  */
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
