7449ee55cf085420529002797f885d31985069f0
katzavben <katzavben@gmail.com>
2022-06-16 03:54:01 +0300

    updated backend block

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 # Configure the Azure provider
 terraform {
   # The following backend block is used in order to store the Terraform state in Azure Blob Storage
-  # Please uncomment the backend block in order to use it
+  # Please uncomment the backend block in order to use it (then use terraform init)
   # ONLY DO IT IF IT'S NOT THE FIRST INIT AND STORAGE ACCOUNT ALREADY CREATED
   /*
   backend "azurerm" {
